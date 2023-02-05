dc9ec6a85e994c525711b93d779ae71ccbf6fe35
Marcos Aparicio <marcos.aparicio593@gmail.com>
2023-02-05 15:16:21 -0500

    editing README.md

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React from "react";
-import {Link} from "react-router-dom";
 
 function Header(){
     return(
@@ -8,15 +7,7 @@ function Header(){
                 <div className="container-fluid flex-column align-items-end">
                     <div className="navbar-collapse collapse">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            {/* <li className="nav-item">
-                                <Link to="/" className="nav-link">Personal Information 1</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/personal-info2" className="nav-link">Personal Information 2</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/summary" className="nav-link">Summary</Link>
-                            </li> */}
+                        {/* TODO: develop a better navbar */}
                         </ul>
                     </div>
                 </div>
